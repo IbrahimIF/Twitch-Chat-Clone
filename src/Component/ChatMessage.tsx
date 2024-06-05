823632cbfad2150a5a28481f35b9a58afd54b578
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-06-05 22:25:26 +0100

    chatMessage format made and badges added

--- src/Component/ChatMessage.tsx
+++ src/Component/ChatMessage.tsx
@@ -2,15 +2,34 @@
 import { MessageModel } from "../utils/models"
 
 type MessageProps = {
-    message:
-}
+    message: MessageModel
+} & React.ComponentPropsWithRef<'div'>
 
-const ChatMessage = () => {
+const ChatMessage = ({message: { author, content }, className, }: MessageProps) => {
+    const Badges = author.badges.map((bg, i) => (
+        <img
+            key={i}
+            src={`/badges/${bg}.png`}
+            className="w-4 h-4 self-center mr-2"
+        />
+    ))
+
+    const Author = (
+        <span className="font-semibold" style={{ color: author.rgbColor }}>
+            {author.username}
+        </span>
+    )
+
+
 
     return 
-      <div>ChatMessage</div>
-    
-
+      <div className={'text-[15px] py-1 px-2 rounded hover:bg-gray-500/30 leading-6 ${className}'}>
+        <div className="inline-flex items-baseline">
+            {Badges}
+            {Author}
+        </div>
+        <span className="ml-3 break-words">{content}</span>
+    </div>
 }
 
 export default ChatMessage
