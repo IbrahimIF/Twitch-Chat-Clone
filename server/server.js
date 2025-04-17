de089c59e1aa16365ef30f2c0bd8e3c50180a032
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-04-17 22:27:58 +0100

    Updated socket.io credentials

--- server/server.js
+++ server/server.js
@@ -11,7 +11,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin:  process.env.FRONTEND_URL || "http://localhost:5173",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: false
   }
 });
 
